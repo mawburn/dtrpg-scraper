81046b8523698d02c9e9401cd055eb2de1aa723c
matt burnett <mawburn7@gmail.com>
2023-09-13 14:19:01 -0400

    basic scraping

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello from ts-node!");
+import { chromium } from 'playwright'
+
+async function scrapeClassContent(url: string): Promise<string | null> {
+  const browser = await chromium.launch({ headless: true })
+  const page = await browser.newPage()
+
+  try {
+    await page.goto(url)
+    const content = await page.textContent('h5.u-text-bold.u-mb-1')
+    return content
+  } catch (error) {
+    console.error('Error fetching or parsing the content:', error)
+    return null
+  } finally {
+    await browser.close()
+  }
+}
+
+// Example usage:
+(async () => {
+  const url = 'https://www.drivethrurpg.com/index.php'
+  const content = await scrapeClassContent(url)
+  console.log(content)
+})()
